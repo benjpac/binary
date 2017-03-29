aa0be9bb0113a1ae80d8cec0130774237a28afca
Ben Metzger and Max Reichman <ben@>
2017-03-29 16:46:40 -0700

    add functionality for convert decimal

--- js/scripts.js
+++ js/scripts.js
@@ -9,10 +9,27 @@ function convertBinary(decimal) {
   alert(binaryStr)
 }
 
+function convertDecimal(binary) {
+  debugger;
+  var binaryArray = binary.toString().split("");
+  var checkNumLength = binary.toString().length;
+  var decimalArray = [256, 128, 64, 32, 16, 8, 4, 2, 1];
+  var splicedDecimalArray = decimalArray.slice(checkNumLength);
+  var decimal = 0;
+  for (var i = 0; i < checkNumLength; i++) {
+    if (binary[i] === "1") {
+      decimal += splicedDecimalArray[i];
+    }
+  }
+  alert(decimal)
+}
+
 $(document).ready(function() {
   $("form").submit(function(event) {
     var numberInput = parseInt($("#number").val());
-    convertBinary(numberInput)
+    var numberInput1 = $("#number1").val();
+    convertBinary(numberInput);
+    convertDecimal(numberInput1)
     event.preventDefault();
   });
 });
